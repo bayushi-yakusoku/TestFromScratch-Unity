bf61c9947b002e26e665873c70ad8d1773f1177c
bayushi-yakusoku <bayushi.tonio@gmail.com>
2021-06-09 19:58:48 +0200

    #1 - Simplifying ReadOnly attribute

--- Assets/alo/Scripts/ReadOnlyAttribute.cs
+++ Assets/alo/Scripts/ReadOnlyAttribute.cs
@@ -27,16 +27,10 @@ public class ReadOnlyDrawer : PropertyDrawer
     /// <param name="label">Label.</param>
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        // Saving previous GUI enabled value
-        var previousGUIState = GUI.enabled;
-
         // Disabling edit for property
         GUI.enabled = false;
 
         // Drawing Property
         EditorGUI.PropertyField(position, property, label);
-
-        // Setting old GUI enabled value
-        GUI.enabled = previousGUIState;
     }
 }
